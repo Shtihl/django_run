7bd4ad17837f1e5441387b76194ff1223318e794
Dmitriy Firsov <shtihlfd@yandex.ru>
2025-10-08 12:25:13 +0300

    Attempt to task001

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def company_details_view(request):
+    company_name = settings.COMPANY_NAME
+    slogan = settings.SLOGAN
+    contacts = settings.CONTACTS
+    if request.method == 'GET':
+        return Response({'company_name': company_name, 'slogan': slogan, 'contacts': contacts})
