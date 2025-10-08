92a2c580bec0d2ebf9d119888b1a607a6ab8a23e
Dmitriy Firsov <shtihlfd@yandex.ru>
2025-10-08 13:22:27 +0300

    Attempt task002

--- app_run/views.py
+++ app_run/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
-
+from rest_framework import viewsets
+from .models import Run
+from .serializers import RunSerializer
 # Create your views here.
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
 
 
 @api_view(['GET'])
